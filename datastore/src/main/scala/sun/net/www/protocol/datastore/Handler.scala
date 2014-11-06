d4033257cf68cf3af64070cd5b66dc1ca2b73001
Dirk Groeneveld <dirkg@allenai.org>
2014-11-06 10:42:45 -0800

    Makes it possible to specify URLs that point into directories that come from the datastore.

--- datastore/src/main/scala/sun/net/www/protocol/datastore/Handler.scala
+++ datastore/src/main/scala/sun/net/www/protocol/datastore/Handler.scala
@@ -2,15 +2,15 @@ package sun.net.www.protocol.datastore
 
 import org.allenai.datastore.Datastore
 
-import sun.net.www.protocol.file.FileURLConnection
-
 import java.net.{URLConnection, URL, URLStreamHandler}
 
 class Handler extends URLStreamHandler {
   override def openConnection(u: URL): URLConnection = {
     val datastore = Datastore(u.getAuthority)
-    val withExtension = """([^/]+)/(.+)-v(\d+)\.(.*)""".r
-    val withoutExtension = """([^/]+)/(.+)-v(\d+)""".r
+    val fileWithExtension = """([^/]+)/(.+)-v(\d+)\.(.*)""".r
+    val fileWithoutExtension = """([^/]+)/(.+)-v(\d+)""".r
+    val directoryWithoutInnerPath = """([^/]+)/(.+)-d(\d+)""".r
+    val directoryWithInnerPath = """([^/]+)/(.+)-d(\d+)/(.*)""".r
 
     // pattern matching on Int
     object Int {
@@ -22,10 +22,14 @@ class Handler extends URLStreamHandler {
     }
 
     val path = u.getPath.stripPrefix("/") match {
-      case withExtension(group, name, Int(version), ext) =>
+      case fileWithExtension(group, name, Int(version), ext) =>
         datastore.filePath(group, s"$name.$ext", version)
-      case withoutExtension(group, name, Int(version)) =>
+      case fileWithoutExtension(group, name, Int(version)) =>
         datastore.filePath(group, name, version)
+      case directoryWithoutInnerPath(group, name, Int(version)) =>
+        datastore.directoryPath(group, name, version)
+      case directoryWithInnerPath(group, name, Int(version), innerPath) =>
+        datastore.directoryPath(group, name, version).resolve(innerPath)
       case _ =>
         throw new IllegalArgumentException(s"$u cannot be parsed as a datastore URI")
     }
