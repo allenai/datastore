4aafa9728b1957f5ef0674aceae67e9750947a10
Michael Schmitz <michael@schmitztech.com>
2014-11-24 10:52:07 -0800

    Use 2.10.4

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import Keys._
 object DatastoreBuild extends Build {
   val buildSettings = Seq(
     organization := "org.allenai",
-    crossScalaVersions := Seq("2.11.4"),
+    crossScalaVersions := Seq("2.10.4"),
     scalaVersion <<= crossScalaVersions { (vs: Seq[String]) => vs.head },
     scalacOptions ++= Seq("-Xlint", "-deprecation", "-unchecked", "-feature"),
     conflictManager := ConflictManager.strict,
