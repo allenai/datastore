849d63cb35b7790ec69107aa01a13a9d02ce13a0
Ashish Sabharwal <AshishS@allenai.org>
2016-06-29 17:40:18 -0700

    add utility methods to get Datastore file/folder path

--- datastore/src/main/scala/org/allenai/datastore/DatastoreUtils.scala
+++ datastore/src/main/scala/org/allenai/datastore/DatastoreUtils.scala
@@ -3,13 +3,28 @@ package org.allenai.datastore
 import org.allenai.common.Logging
 
 import java.io.File
+import java.nio.file.Path
 
 import scala.io.{ BufferedSource, Codec, Source }
 
-/** Various convenient utiltiies for accessing the Datastore. */
+/** Various convenient utilities for accessing the Datastore. */
 object DatastoreUtils extends Logging {
 
-  /** Get a datastore file as a buffered Source. Caller is responsible for closing this stream. */
+  /** Get a Datastore file path. */
+  def getDatastoreFilePath(
+    datastoreName: String,
+    group: String,
+    name: String,
+    version: Int
+  ): Path = Datastore(datastoreName).filePath(group, name, version)
+
+  /** Get a Datastore file path, given a URI. */
+  def getDatastoreFilePath(datastoreUri: String): Path = {
+    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUri)
+    getDatastoreFilePath(datastoreName, group, name, version)
+  }
+
+  /** Get a Datastore file as a buffered Source. Caller is responsible for closing this stream. */
   def getDatastoreFileAsSource(
     datastoreName: String,
     group: String,
@@ -17,17 +32,31 @@ object DatastoreUtils extends Logging {
     version: Int
   )(implicit codec: Codec): BufferedSource = {
     logger.debug(s"Loading file from $datastoreName datastore: $group/$name-v$version")
-    val file = Datastore(datastoreName).filePath(group, name, version).toFile
+    val file = getDatastoreFilePath(datastoreName, group, name, version).toFile
     Source.fromFile(file)(codec)
   }
 
-  /** Get a datastore file as a buffered Source. Caller is responsible for closing this stream. */
-  def getDatastoreFileAsSource(datastoreUriString: String): BufferedSource = {
-    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUriString)
-    getDatastoreFileAsSource(datastoreName, group, name, version)
+  /** Get a Datastore file as a buffered Source. Caller is responsible for closing this stream. */
+  def getDatastoreFileAsSource(datastoreUri: String)(implicit codec: Codec): BufferedSource = {
+    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUri)
+    getDatastoreFileAsSource(datastoreName, group, name, version)(codec)
   }
 
-  /** Get a datastore directory as a folder. */
+  /** Get a Datastore directory path. */
+  def getDatastoreDirectoryPath(
+    datastoreName: String,
+    group: String,
+    name: String,
+    version: Int
+  ): Path = Datastore(datastoreName).directoryPath(group, name, version)
+
+  /** Get a Datastore directory path, given a URI. */
+  def getDatastoreDirectoryPath(datastoreUri: String): Path = {
+    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUri)
+    getDatastoreDirectoryPath(datastoreName, group, name, version)
+  }
+
+  /** Get a Datastore directory as a folder. */
   def getDatastoreDirectoryAsFolder(
     datastoreName: String,
     group: String,
@@ -35,12 +64,12 @@ object DatastoreUtils extends Logging {
     version: Int
   ): File = {
     logger.debug(s"Loading directory from $datastoreName datastore: $group/$name-v$version")
-    Datastore(datastoreName).directoryPath(group, name, version).toFile
+    getDatastoreDirectoryPath(datastoreName, group, name, version).toFile
   }
 
-  /** Get a datastore directory as a folder */
-  def getDatastoreDirectoryAsFolder(datastoreUriString: String): File = {
-    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUriString)
+  /** Get a Datastore directory as a folder, given a URI. */
+  def getDatastoreDirectoryAsFolder(datastoreUri: String): File = {
+    val (datastoreName, group, name, version) = parseDatastoreUri(datastoreUri)
     getDatastoreDirectoryAsFolder(datastoreName, group, name, version)
   }
 
@@ -52,12 +81,12 @@ object DatastoreUtils extends Logging {
     * datastore://private/org.allenai.aristo.tables/Grade4-v10.json  (with extension)
     * datastore://private/org.allenai.aristo.tabledata/tables-v4  (without extension)
     */
-  def parseDatastoreUri(datastoreUriString: String): (String, String, String, Int) = {
-    datastoreUriString match {
+  def parseDatastoreUri(datastoreUri: String): (String, String, String, Int) = {
+    datastoreUri match {
       case datastoreUriRegex(datastoreName, group, basename, version, extension) =>
         val ext = if (extension == null) "" else extension // extension is optional
         (datastoreName, group, basename + ext, version.toInt)
-      case _ => throw new IllegalArgumentException(s"Cannot parse $datastoreUriString")
+      case _ => throw new IllegalArgumentException(s"Cannot parse $datastoreUri")
     }
   }
 }
