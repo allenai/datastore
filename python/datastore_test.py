e30153441a76f3c59d237c2214af9c0550bce482
Dirk Groeneveld <dirkg@allenai.org>
2019-01-07 17:57:57 -0800

    Fixes tests

--- python/datastore_test.py
+++ python/datastore_test.py
@@ -6,10 +6,10 @@ class TestDatastore(unittest.TestCase):
     """These tests require access to the public AI2 datastore."""
 
     def test_download_file(self):
-        p = datastore.public.filePath("org.allenai.datastore", "DatastoreCli.jar", 1)
+        p = datastore.public.file("org.allenai.datastore", "DatastoreCli.jar", 1)
         assert p.is_file()
 
     def test_download_directory(self):
-        p = datastore.public.directoryPath("org.allenai.datastore", "TestDirectory", 1)
+        p = datastore.public.directory("org.allenai.datastore", "TestDirectory", 1)
         assert p.is_dir()
         assert (p / "1.gif").is_file()
